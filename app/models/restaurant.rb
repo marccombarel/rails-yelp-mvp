5bda35b8af25df1be6ba2a7442e7d8db27fb5d6a
Marc Combarel <marc.combarel@edhec.com>
2020-02-12 16:36:20 +0100

    added restaurants controller + all restaurants views + all restaurants methods and simple_form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,9 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w(chinese italian japanese french belgian)
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = [
+  { name: 'Le bon poulet', address: '119 rue nationale, Lille', phone_number: '0671139829', category: 'french'},
+  { name: 'Le Shiba', address: '27 rue des trucs pas bons, Lille', phone_number: '0728134529', category: 'chinese'},
+  { name: 'Le Babe', address: '50 rue hopital militaire, Lille', phone_number: '0783544529', category: 'french'},
+  { name: 'Le Barbier qui fume', address: 'JSP et c pas grave, Lille', phone_number: '0783825469', category: 'french'},
+  { name: 'Le Jomon de Negishi', address: 'Gna GnaGna pas grave, Lille', phone_number: '0783825469', category: 'japanese'}
+]
+
+restaurants.each { |restaurant| Restaurant.new(restaurant).save}
