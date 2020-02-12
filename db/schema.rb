71d3a7d4a43ddc1dcab2b8a8f1dcbe3a6b057924
Marc Combarel <marc.combarel@edhec.com>
2020-02-12 12:13:46 +0100

    added restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_12_111307) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "category"
+    t.string "reviews"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.string "restaurant"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
